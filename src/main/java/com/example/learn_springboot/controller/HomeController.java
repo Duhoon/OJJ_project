0edf8fe923ef608842c3ef485173234a40d0aad1
jeky22 <qhzh33@gmail.com>
2019-08-27 10:32:51 +0900

    make login

--- src/main/java/com/example/learn_springboot/controller/HomeController.java
+++ src/main/java/com/example/learn_springboot/controller/HomeController.java
@@ -1,12 +1,28 @@
 package com.example.learn_springboot.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController{
-    @RequestMapping(value = "/home")
-        public String ActionMethod(){
-            return "/home";
+public class HomeController {
+    @RequestMapping(value = "/home/{action}")
+    public ModelAndView hi(@RequestParam Map<String, Object> paramMap, @PathVariable String action, ModelAndView modelAndView) {
+      
+        if (paramMap.get("isLogin") == null) {
+            paramMap.put("isLogin", false);
         }
+      
+        modelAndView.setViewName("/home/"+action);
+        modelAndView.addObject("data", paramMap);
+
+        return modelAndView;
+    }
+
+   
+
 }
